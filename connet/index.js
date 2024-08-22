a17287bb7084ebac3cf09d420cc4731d4bdc5837
kananis231 <kananisharvil603@gmail.com>
2024-08-22 18:04:09 +0530

    edit

--- connet/index.js
+++ connet/index.js
@@ -6,13 +6,29 @@ const db = require("./config/database");
 const crudSchema = require("./model/crudSchema");
 
 app.use(express.urlencoded())
+
 app.set("view engine","ejs");
-app.get("/",(req,res)=>{
-    res.render("index");
+app.get("/", async(req,res)=>{
+ let data = await crudSchema.find({});
+    res.render("index",{data});
 })
-app.post("/insert",(req,res)=>{
-    console.log(req.body);
+app.post("/insert", async(req,res)=>{
+    let data = await crudSchema.create(req.body);
+    res.redirect("/");
 })
+
+app.get("/delete", async(req,res)=>{
+    let data = await crudSchema.findByIdAndDelete(req.query.id);
+    res.redirect("/");
+})  
+app.get("/edit", async(req,res)=>{
+    let data = await crudSchema.findById(req.query.id);
+    res.render("edit",{data});
+})
+app.post("/editData", async(req,res)=>{
+    let data = await crudSchema.findByIdAndUpdate(req.query.id,req.body);
+    res.redirect("/");
+}) 
 app.listen(port, (err) => {
     err ? console.log(err) : console.log(`server started on port ${port}`);
 })
